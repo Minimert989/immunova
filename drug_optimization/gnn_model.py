5877e4c0bf2639634522e5fe7519f8b6e001147c
ohtherewegoagain <pooja892892@gmail.com>
2025-04-06 00:21:14 +0530

    Update gnn_model.py

--- drug_optimization/gnn_model.py
+++ drug_optimization/gnn_model.py
@@ -1,10 +1,18 @@
-# GNN for drug design
-import torch.nn as nn
+# drug_optimization/gnn_model.py
 
-class DrugGNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc = nn.Linear(100, 2)
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
 
-    def forward(self, x):
-        return self.fc(x)
+class DrugGNN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(DrugGNN, self).__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, out_channels)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training, p=0.3)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
